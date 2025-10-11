42018af7ff6c9eb6d98db96d797692e9030d0733
leotola1325 <leotola1325@gmail.com>
2025-10-11 12:14:56 -0300

    anotações

---
+++ aula4/anotações.py
@@ -0,0 +1,27 @@
+# ==========================    principais funções ===========================================================
+
+ # logica 
+ # variaveis 
+
+ # tipos de primitivos
+
+ # dados texto = 'texto'  string = str ()
+ # dados real = 5.0  float  floas()
+ # dados boleano =  true -- boll boll()
+
+
+ # sinais aritimeticos 
+ # + - * / // % **0.5
+
+
+ # ========================== teste dobro de barra e rsto =============================================================
+ # n1 = 25
+ # n2 = 5
+ # print(n1//n2)   divisao com duas barras mostra o resultado sem a casa decimal somete o inteiro  (ex resultado e 4.5 com duas barras ele mostra somente 4)
+ # print(n1%n2)
+
+
+# # =========================== selecionar todos o codigo ===================================================
+ 
+# #  ctrl + A == seleciona todo o conteudo 
+# quero alteral todas as variavei co  mesmo numero  == selecionar a variavel ctrl+ D  e mudar o nome da variavel em todos locias que ela foi usada
